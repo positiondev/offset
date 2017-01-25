c8b45766fd8215ec35d76693002c200030c39254
Libby Horacek <libby@daydrea.me>
2017-01-25 16:15:03 -0500

    Test wpCustomDate Fill separately from wpCustom

--- spec/Main.hs
+++ spec/Main.hs
@@ -266,23 +266,17 @@ larcenyFillTests = do
       "<wpCustom endpoint=\"dev/null\"><wpThisIsNull /></wpCustom>" `shouldRender` "<!-- JSON field found, but value is null. -->"
   describe "<wpCustomDate>" $ do
     it "should parse a date field with the format string it's given" $
-      "<wpCustom endpoint=\"jacobin/featured-content/editors-picks\" > \
-      \   <wpCustomDate date=\"${wpPostDate}\" wp_format=\"%Y-%m-%d %H:%M:%S\"> \
-      \     <wpDay />~<wpMonth />~<wpYear /> \
-      \   </wpCustomDate> \
-      \ </wpCustom>" `shouldRender` "26~04~2013"
+      "<wpCustomDate date=\"2013-04-26 10:11:52\" wp_format=\"%Y-%m-%d %H:%M:%S\"> \
+      \   <wpDay />~<wpMonth />~<wpYear /> \
+      \ </wpCustomDate>" `shouldRender` "26~04~2013"
     it "should format a date field with the format strings it's given" $
-      "<wpCustom endpoint=\"jacobin/featured-content/editors-picks\" > \
-      \   <wpCustomDate date=\"${wpPostDate}\" wp_format=\"%Y-%m-%d %H:%M:%S\"> \
-      \     <wpMonth format_out=\"%B\"/> <wpDay format_out=\"%-d\"/>, <wpYear /> \
-      \   </wpCustomDate> \
-      \ </wpCustom>" `shouldRender` "April 26, 2013"
+      "<wpCustomDate date=\"2013-04-26 10:11:52\" wp_format=\"%Y-%m-%d %H:%M:%S\"> \
+      \   <wpMonth format_out=\"%B\"/> <wpDay format_out=\"%-d\"/>, <wpYear /> \
+      \ </wpCustomDate>" `shouldRender` "April 26, 2013"
     it "should use default WordPress date format if none specified" $
-      "<wpCustom endpoint=\"jacobin/featured-content/editors-picks\" > \
-      \   <wpCustomDate date=\"${wpPostDate}\"> \
-      \     <wpDay />~<wpMonth />~<wpYear /> \
-      \   </wpCustomDate> \
-      \ </wpCustom>" `shouldRender` "26~04~2013"
+      "<wpCustomDate date=\"2013-04-26 10:11:52\"> \
+      \    <wpDay />~<wpMonth />~<wpYear /> \
+      \ </wpCustomDate>" `shouldRender` "26~04~2013"
 
 
 
