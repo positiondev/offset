0aaca006df9574c87963a9d1e397aa691a942f57
Libby Horacek <libby@daydrea.me>
2017-08-22 12:06:12 -0400

    Allow Feeds to specific how to get authors of posts
    Feeds specify a WPAuthorStyle to say if they are using inline guest authors or
    the default author-user style.

--- spec/Main.hs
+++ spec/Main.hs
@@ -41,7 +41,7 @@ main = runTests
 
 feedTests :: Spec
 feedTests =
-  describe "rss feed" $
+  describe "rss feed" $ do
     it "should make a feed" $ do
       ctxt <- initFauxRequestNoCache
       let wpfeed = WPFeed
@@ -51,6 +51,7 @@ feedTests =
                      Nothing
                      "https://myurl.com"
                      buildEntryLinks
+                     GuestAuthors
                      (renderFeedContent ctxt)
       ft <- toXMLFeed (_wordpress ctxt) wpfeed
       ft `shouldBe` "<?xml version='1.0' ?>\n<feed xmlns=\"http://www.w3.org/2005/Atom\">\n  <id>https://myurl.com/feed</id>\n  <title type=\"text\">My Blog</title>\n  <updated>2014-10-20T07:00:00Z</updated>\n  <entry>\n    <id>https://myurl.com/2014/10/foo-bar/</id>\n    <title type=\"html\">&lt;i&gt;Foo&lt;/i&gt; bar</title>\n    <updated>2014-10-20T07:00:00Z</updated>\n    <published>2014-10-20T07:00:00Z</published>\n    <summary type=\"html\">summary</summary>\n    <content type=\"html\">This is the title: &lt;i&gt;Foo&lt;/i&gt; bar</content>\n    <author>\n      <name>Emma Goldman</name>\n    </author>\n    <link href=\"https://myurl.com/2014/10/foo-bar/\" title=\"&lt;i&gt;Foo&lt;/i&gt; bar\" />\n  </entry>\n</feed>\n"
